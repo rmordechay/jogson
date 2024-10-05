1f2150fbfb082bac504d334b6733e992ce2aede4
r.mordechay <r.mordechay@gridx.de>
2024-10-06 00:35:16 +0200

    Second commit

--- main.go
+++ main.go
@@ -7,16 +7,31 @@ import (
 
 func main() {
 	const jsonStr = `
-  {
-  	"field1": 0,
-  	"field2": 0,
-  	"field3": {
-			"field1": 5
+    {
+  		"field1": [1, 2],
+  		"field3": {
+			"field1": 5.5,
+			"field2": [
+				{"name": "roi"},
+				{"name": "adi"}
+			]
+		},
+  		"field4": "null",
+  		"field5": 6,
+  		"field6": 7
+	}`
+	const jsonStr1 = `[{"roi": 2}, {"roi": 2}]`
+	mapperObject, err := mapper.GetMapper(jsonStr)
+	if err != nil {
+		panic(err)
+	}
+	t := mapperObject.AsObject.Get("field3").AsObject.Get("field2").AsArray
+	//get := t.Get("field3").AsObject
+	for _, v := range t.Elements() {
+		object := v.AsObject
+		for k1, v1 := range object.Elements() {
+			fmt.Println(k1)
+			fmt.Println(v1)
 		}
 	}
-	`
-	jsonMapper := mapper.CreateObjectMapperFromStr(jsonStr)
-	int1 := jsonMapper.GetByName("field3").GetByName("field1").AsInt()
-	int2 := jsonMapper.GetByName("field1").AsInt()
-	fmt.Println(int1 + int2)
 }
--- mapper/json_object.go
+++ mapper/json_object.go
@@ -1,49 +1,186 @@
 package mapper
 
 import (
-	"strconv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"reflect"
 )
 
-type Json interface {
-	GetByName(key string) Json
-	Get(i int) Json
-	Has(key string) bool
-	AsString() string
-	AsInt() int
+type JsonType int
+
+const (
+	Bool JsonType = iota
+	Int
+	Float
+	String
+	Object
+	Array
+	Null
+)
+
+type Json struct {
+	IsBool   bool
+	IsInt    bool
+	IsFloat  bool
+	IsString bool
+	IsObject bool
+	IsArray  bool
+	IsNull   bool
+
+	AsBool   bool
+	AsInt    int
+	AsFloat  float64
+	AsString string
+	AsObject JsonObject
+	AsArray  JsonArray
+}
+
+func (j Json) getType() JsonType {
+	if j.IsBool {
+		return Bool
+	} else if j.IsInt {
+		return Int
+	} else if j.IsFloat {
+		return Float
+	} else if j.IsString {
+		return String
+	} else if j.IsObject {
+		return Object
+	} else if j.IsArray {
+		return Array
+	}
+	return Null
+}
+
+func (j Json) String() string {
+	switch j.getType() {
+	case Bool:
+		return fmt.Sprintf("%v", j.AsBool)
+	case Int:
+		return fmt.Sprintf("%v", j.AsInt)
+	case Float:
+		return fmt.Sprintf("%v", j.AsFloat)
+	case String:
+		return fmt.Sprintf("%v", j.AsString)
+	case Object:
+		return fmt.Sprintf("%v", j.AsObject)
+	case Array:
+		return fmt.Sprintf("%v", j.AsArray)
+	case Null:
+		return fmt.Sprintf("%v", nil)
+	}
+	return ""
+}
+
+type JsonArray struct {
+	elements []interface{}
 }
 
 type JsonObject struct {
 	object map[string]interface{}
-	value  []byte
 }
 
-func (o JsonObject) Get(i int) Json {
-	panic("implement me")
-}
-
-func (o JsonObject) Has(key string) bool {
-	for k := range o.object {
-		if k == key {
-			return true
-		}
-	}
-	return false
-}
-
-func (o JsonObject) GetByName(key string) Json {
+func (o JsonObject) Get(key string) Json {
 	for k, v := range o.object {
 		if k == key {
-			return createObjectMapper(v)
+			return getMapperFromField(v)
 		}
 	}
-	return o
+	return Json{}
 }
 
-func (o JsonObject) AsString() string {
-	return string(o.value)
+func GetMapper(data string) (Json, error) {
+	var mapper Json
+	if isJsonArray(data) {
+		mapper.IsArray = true
+		array, err := parseJsonArray(data)
+		if err != nil {
+			return Json{}, err
+		}
+		mapper.AsArray = array
+	} else {
+		mapper.IsObject = true
+		object, err := parseJsonObject(data)
+		if err != nil {
+			return Json{}, err
+		}
+		mapper.AsObject = object
+	}
+	return mapper, nil
 }
 
-func (o JsonObject) AsInt() int {
-	toInt, _ := strconv.Atoi(string(o.value))
-	return toInt
+func getMapperFromField(data interface{}) Json {
+	var mapper Json
+	switch data.(type) {
+	case bool:
+		mapper.IsBool = true
+		mapper.AsBool = data.(bool)
+	case float64:
+		if data == float64(int(data.(float64))) {
+			mapper.IsInt = true
+			mapper.AsInt = int(data.(float64))
+		} else {
+			mapper.IsFloat = true
+		}
+		mapper.AsFloat = data.(float64)
+	case string:
+		mapper.IsString = true
+		mapper.AsString = data.(string)
+	case map[string]interface{}:
+		mapper.IsObject = true
+		var jo JsonObject
+		jo.object = data.(map[string]interface{})
+		mapper.AsObject = jo
+	case []interface{}:
+		mapper.IsArray = true
+		var ja JsonArray
+		ja.elements = data.([]interface{})
+		mapper.AsArray = ja
+	case nil:
+		mapper.IsNull = true
+	default:
+		log.Fatalf("%v not implemented", reflect.TypeOf(data))
+	}
+	return mapper
+}
+
+func isJsonArray(data string) bool {
+	return data[0] == '['
+}
+
+func (a JsonArray) Elements() []Json {
+	jsons := make([]Json, 0, len(a.elements))
+	for _, element := range a.elements {
+		jsons = append(jsons, getMapperFromField(element))
+	}
+	return jsons
+}
+
+func (o JsonObject) Elements() map[string]Json {
+	jsons := make(map[string]Json)
+	for k, v := range o.object {
+		jsons[k] = getMapperFromField(v)
+	}
+	return jsons
+}
+
+func parseJsonObject(data string) (JsonObject, error) {
+	var jo JsonObject
+	err := json.Unmarshal([]byte(data), &jo.object)
+	if err != nil {
+		return JsonObject{}, err
+	}
+	return jo, nil
+}
+
+func parseJsonArray(data string) (JsonArray, error) {
+	var ja JsonArray
+	var arr []interface{}
+	err := json.Unmarshal([]byte(data), &arr)
+	if err != nil {
+		return JsonArray{}, err
+	}
+	ja.elements = arr
+	return ja, nil
 }
